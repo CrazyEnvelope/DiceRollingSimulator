e912639ede5fbf59b88f88a4c29330f1da0442b1
Alex <stanciustefan500@gmail.com>
2025-08-06 22:30:48 +0300

    Create a functional window for adding players.

--- enterplayers.py
+++ enterplayers.py
@@ -10,10 +10,6 @@ class EnterPlayers(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        #reading data from json
-        with open('playerdata.json','r') as file:
-            playerData = json.load(file)
-
         #window setup
         widthW, heightW = (400, 300)
         self.title('Enter Players')
@@ -24,23 +20,50 @@ class EnterPlayers(tk.Tk):
         self.resizable(0, 0)
 
         #info screen
-        labelInfo = tk.Label(self, text = "Note! Add user by entering their's name in the filed below,\n or select them from the existing database!", font = ("Helvetica",10),background="#2b2b2a", foreground="white")
-        labelInfo.place(relx=0.1,rely=0.1,anchor = tk.NW)
+        self.labelInfo = tk.Label(self, text = "Note! Add user by entering their's name in the filed below!\n The database will be erased each time of playing.", font = ("Helvetica",10),background="#2b2b2a", foreground="white")
+        self.labelInfo.place(relx=0.1,rely=0.1,anchor = tk.NW)
 
         #combobox setup
-        playersFromFile = []
-        for player in playerData:
-            playersFromFile.append(player["playername"])
-        combobox = ttk.Combobox(self,values=playersFromFile)
-        combobox.config(width=30)
-        combobox.set("Select an existing user")
-        combobox.place(relx=0.1,rely=0.49)
+        global presentPlayers
+        self.combobox = ttk.Combobox(self,values=presentPlayers)
+        self.combobox.config(width=30)
+        self.combobox.set("Users added can be seen here!")
+        self.combobox.place(relx=0.1,rely=0.49)
 
         # textbox setup
-        textbox = tk.Text(self, height=1, width=25)
-        textbox.place(relx=0.1, rely=0.3,anchor = tk.NW)
+        self.textbox = tk.Text(self, height=1, width=25)
+        self.textbox.place(relx=0.1, rely=0.3,anchor = tk.NW)
 
         #button to add user
-        addUserButton = tk.Button(self, text="Add user", font=("Helvetica",10))
-        addUserButton.place(relx=0.9,rely=0.29, anchor = tk.NE)
+        self.addUserButton = tk.Button(self, text="Add user", font=("Helvetica",10))
+        self.addUserButton.place(relx=0.9,rely=0.29, anchor = tk.NE)
+
+        # button to start the game
+        self.startButton = tk.Button(self, text="Start", font=("Helvetica", 10))
+        self.startButton.place(relx=0.9, rely=0.48, anchor=tk.NE)
+        self.startButton.config(width=6)
+
+
+    def enterPlayers(self):
+        global presentPlayers
+        playerFromTextBox = self.textbox.get(1.0,"end-1c")
+        presentPlayers.append(playerFromTextBox)
+        self.combobox.configure(values=presentPlayers)
+
+        presentPlayersDict = []
+        for player in presentPlayers:
+            presentPlayersDict.append(
+                {
+                    "rank": 0,
+                    "playername": player,
+                    "totalscore": 0,
+                    "rolls": 0,
+                    "lastplayed": "2000-01-01 00:00"
+                }
+            )
+
+        with open('playerdata.json', 'w') as file:
+            json.dump(presentPlayersDict,file,indent=4)
+
+
 
--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import app
 import leaderboard
 import enterplayers
 
-#def openLeaderBoard():
-#    leaderboardApp = leaderboard.Leaderboard()
+def openLeaderBoard():
+    leaderboardApp = leaderboard.Leaderboard()
+
+def startGame():
+    diceApp = app.App()
+    diceApp.rollButton.configure(command=diceApp.rollDice)
+    diceApp.leaderBoardButton.configure(command=openLeaderBoard)
+    diceApp.mainloop()
 
 if __name__ == "__main__":
     enterplayersApp = enterplayers.EnterPlayers()
+    enterplayersApp.addUserButton.configure(command=enterplayersApp.enterPlayers)
+    enterplayersApp.startButton.configure(command=startGame)
     enterplayersApp.mainloop()
 
-    #diceApp = app.App()
-    #diceApp.rollButton.configure(command=diceApp.rollDice)
-    #diceApp.leaderBoardButton.configure(command=openLeaderBoard)
-    #diceApp.mainloop()
